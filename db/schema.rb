23abfc05f460dcf985afc3af0e386f28bd2afaa2
Cray <craymaru@gmail.com>
2020-02-25 21:43:08 +0900

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_02_25_123641) do
 
-ActiveRecord::Schema.define(version: 2020_02_25_025125) do
+  create_table "keymaps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "origin_keymap_id"
+    t.string "name"
+    t.string "version"
+    t.text "introduction"
+    t.integer "status"
+    t.datetime "modified_at"
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_02_25_025125) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
