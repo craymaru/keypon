d4b3b164cca168ccecf8736f9ea58527aac7e06c
Cray <craymaru@gmail.com>
2020-02-26 00:16:50 +0900

    Added: Create additional columns to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_150233) do
+ActiveRecord::Schema.define(version: 2020_02_25_151125) do
 
   create_table "commands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "keymap_id"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(version: 2020_02_25_150233) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name"
+    t.string "biography"
+    t.string "user_image_id"
+    t.boolean "is_official"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
