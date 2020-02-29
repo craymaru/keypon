68b4bb7c98ac3ce59a8ce4dae1b688ac93fb75c2
Cray <craymaru@gmail.com>
2020-02-29 15:17:03 +0900

    Editing: commands controller

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,9 +1,15 @@
 class CommandsController < ApplicationController
   def create
-    command = Command.new()
-    keybind = Keybinding.new()
+    command = Command.new
 
-    if command.save && keybind.save
+
+    current_end_user.cart_items.each do |cart_item|
+      decided_order.order_details.build(
+        name: cart_item.item.name
+      )
+    end
+    
+    if command.save
     else
     end
   end
