35da12ab6a49881a704fe91c53c84904ee1b027d
Cray <craymaru@gmail.com>
2020-03-09 21:46:05 +0900

    Create image upload function. Updated .gitignore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :name, :email, :biography, :user_image_id)
+    params.require(:user).permit(:username, :name, :email, :biography, :image)
     # params.require(:user).permit(:username, :name, :email, :password, :biography, :user_image_id)
   end
 end
