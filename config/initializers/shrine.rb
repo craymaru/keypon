3899468adafe46ba1adb97ce418e2c636613c3c7
Cray <craymaru@gmail.com>
2020-03-13 06:55:55 +0900

    Changed Shrine to save to S3 in production env.

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,13 +1,30 @@
 # REQUIRES
 require "shrine"
 require "shrine/storage/file_system"
+require "shrine/storage/s3"
 
 # UPLOAD DIRECTRY
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),
-}
+# PRODUCTION - S3
+if Rails.env.production?
+  s3_options = {
+    access_key_id: "your_access_key_id",
+    secret_access_key: "your_secret_access_key",
+    region: "ap-northeast-1",
+    bucket: "abc.example.com",
+  }
+
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options),
+  }
+else
+  # LOCAL
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "images/uploads/cache"),
+    store: Shrine::Storage::FileSystem.new("public", prefix: "images/uploads/store"),
+  }
+end
 
 # DEFINE PLUGINS
 Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data
+Shrine.plugin :cached_attachment_data # for forms
