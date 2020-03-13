80515cca6458e24e5d67ee88d92708232ce23b46
Cray <craymaru@gmail.com>
2020-03-13 17:43:37 +0900

    Testing js webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,26 @@
 const { environment } = require("@rails/webpacker");
-
+const merge = require("webpack-merge");
+const webpack = require("webpack");
 // Require jQuery and Bootstap javascripts
 const webpack = require("webpack");
 environment.plugins.prepend(
   "Provide",
   new webpack.ProvidePlugin({
-    $: "jquery/dist/jquery",
-    jQuery: "jquery/dist/jquery",
-    jquery: "jquery/dist/jquery",
-    Popper: 'popper.js/dist/popper'
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+    "window.jQuery": "jquery",
+    Popper: ["popper.js", "default"] // for Bootstrap 4
   })
 );
-module.exports = environment;
+
+const envConfig = (module.exports = environment);
+const aliasConfig = (module.exports = {
+  resolve: {
+    alias: {
+      jquery: "jquery/src/jquery"
+    }
+  }
+});
+
+module.exports = merge(envConfig.toWebpackConfig(), aliasConfig);
