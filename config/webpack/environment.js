fcddebbcbda7e6bad7e1b6612bc2ddc3124e0bf2
Cray <craymaru@gmail.com>
2020-03-13 17:07:41 +0900

    Modified webpack/jquery path for production env

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,12 +5,10 @@ const webpack = require("webpack");
 environment.plugins.prepend(
   "Provide",
   new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    jquery: "jquery",
-    "window.jQuery": "jquery",
-    // "jQuery.tagsinput": "bootstrap-tagsinput",
-    Popper: "popper.js"
+    $: "jquery/dist/jquery",
+    jQuery: "jquery/dist/jquery",
+    jquery: "jquery/dist/jquery",
+    Popper: 'popper.js/dist/popper'
   })
 );
 module.exports = environment;
